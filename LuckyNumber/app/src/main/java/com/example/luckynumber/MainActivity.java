edcdef1d7b8b43acf1a9d8f2313192f832c2558a
duynguyen <duy.nguyen@mga.edu>
2024-02-12 22:29:23 -0500

    planetapp

--- LuckyNumber/app/src/main/java/com/example/luckynumber/MainActivity.java
+++ LuckyNumber/app/src/main/java/com/example/luckynumber/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.luckynumber;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +36,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId=item.getItemId();
+        if(itemId== R.id.home){
+            Toast.makeText(
+                    this,
+                    "You selected Home",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }else if(itemId==R.id.search){
+            Toast.makeText(
+                    this,
+                    "You selected Search",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
