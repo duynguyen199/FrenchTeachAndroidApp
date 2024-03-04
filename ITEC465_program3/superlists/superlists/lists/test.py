d18864705ed5975e0c15b403a708a97c958c0e8d
duynguyen <duy.nguyen@mga.edu>
2024-03-04 11:55:59 -0500

    abc

---
+++ ITEC465_program3/superlists/superlists/lists/test.py
@@ -0,0 +1,11 @@
+# from django.test import TestCase
+# from django.urls import resolve
+# from django.views import home_page
+from django.shortcuts import render
+
+# class HomePageTest(TestCase):
+#     def test_roof_url_resolves_to_home_page_view(self):
+#         found = resolve('/')
+#         self.assertEqual(found.func, home_page)
+def home_page():
+    pass
